6c2b4592e41fb1c2e2d5baee0258623ad6dd2bd4
DinisB <dinisbarroso.dev@gmail.com>
2025-02-27 22:03:11 +0000

    Exercicio 1 resolvido

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro: ");
+            sbyte.TryParse(Console.ReadLine(), out sbyte n);
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+        }
+    }
+}
