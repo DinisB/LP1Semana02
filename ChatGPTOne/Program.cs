db3fb54806661be4b0640769b49305e8dc875c76
DinisB <dinisbarroso.dev@gmail.com>
2025-02-27 23:18:48 +0000

    Exercicio 10 - Questão 1

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Escreve uma pergunta");
+            string input = Console.ReadLine();
+            switch (input) 
+            {
+                case "que vitamina dão as bananas?":
+                    Console.WriteLine("potássio");
+                    break;
+            }
+        }
+    }
+}
