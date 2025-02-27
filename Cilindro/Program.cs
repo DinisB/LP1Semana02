de3567aed82a3dd7662b2c5f204cf9828da3f7f5
DinisB <dinisbarroso.dev@gmail.com>
2025-02-27 22:37:43 +0000

    Inicio exercicio 5

---
+++ Cilindro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Altura: ");
+            double.TryParse(Console.ReadLine(), out double altura);
+            Console.Write("Raio: ");
+            double.TryParse(Console.ReadLine(), out double raio);
+        }
+    }
+}
