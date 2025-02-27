29f27dd622556b8f0da42fc82055aef36a0bd05b
DinisB <dinisbarroso.dev@gmail.com>
2025-02-27 22:25:37 +0000

    Exercicio 4 feito

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro-não negativo: ");
+            byte.TryParse(Console.ReadLine(), out byte n);
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n ^ 6);
+            Console.WriteLine(n > 10);
+        }
+    }
+}
