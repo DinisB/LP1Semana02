e3292d6087544ce7ef1f862150eec7c08a38041b
DinisB <dinisbarroso.dev@gmail.com>
2025-02-27 23:26:27 +0000

    Exercicio 10 - Resto das perguntas

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,13 +6,30 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            Console.Write("Escreve uma pergunta");
+            while (true) {
+            Console.Write("Escreve uma pergunta (EXIT para sair): ");
             string input = Console.ReadLine();
+            if (input == "exit")
+                break;
+            
             switch (input) 
             {
                 case "que vitamina dão as bananas?":
                     Console.WriteLine("potássio");
                     break;
+                case "qual é o queijo mais rançoso?":
+                    Console.WriteLine("vieux boulogne");
+                    break;
+                case "quanto é 0 a dividir por 0?":
+                    Console.WriteLine("/bin/rm -f file.log");
+                    break;
+                case "quem é a coisa mais veloz de sempre?":
+                    Console.WriteLine("o faisca ou o sonic");
+                    break;
+                default:
+                    Console.WriteLine("Não entendi, podes repetir?");
+                    break;
+            }
             }
         }
     }
