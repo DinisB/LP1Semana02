410294e6868c36f681456b93aa210eebd4391b40
DinisB <dinisbarroso.dev@gmail.com>
2025-02-27 22:38:03 +0000

    Final exercicio 5

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -10,6 +10,10 @@ namespace Cilindro
             double.TryParse(Console.ReadLine(), out double altura);
             Console.Write("Raio: ");
             double.TryParse(Console.ReadLine(), out double raio);
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+            double superficie = 2 * Math.PI * raio * (raio + altura);
+            Console.WriteLine("Volume do cilindro: {0:F3}", volume);
+            Console.WriteLine("Area da superficie do cilindro: {0:F3}", superficie);
         }
     }
 }
