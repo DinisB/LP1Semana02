03c927e6d3683315e9f3c68f6001eb9d6e9665cd
DinisB <dinisbarroso.dev@gmail.com>
2025-02-28 14:16:07 +0000

    Parte do exercicio

---
+++ PerfectCountdown/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace PerfectCountdown
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Start Number: ");
+            int.TryParse(Console.ReadLine(), out int start);
+            Console.Write("Step Number: ");
+            int.TryParse(Console.ReadLine(), out int step);
+
+            if (start <= 1){
+                Console.WriteLine("Out-of-range start or step. Try again.");
+            }
+            else if (step >= start){
+                Console.WriteLine("Start must be higher than step. Try again.");
+            }
+            else if (start % step != 0) {
+                Console.WriteLine("Start not divisible by step. Try again.");
+            }
+        }
+    }
+}
